01b174b19bab7f651d65d56bfc560caa1129be5e
kenn <bos@kenn.cn>
2018-05-17 21:36:31 +0800

    add activeMQ

---
+++ kir-queues/src/main/java/com/ken/QueuesApplication.java
@@ -0,0 +1,12 @@
+package com.ken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuesApplication.class, args);
+	}
+}
