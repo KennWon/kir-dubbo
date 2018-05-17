2577d1f243946ec7db2140c3dcbc047cec2a7f75
kenn <bos@kenn.cn>
2018-05-17 20:54:15 +0800

    mondify bug for mongodb

--- kir-web/src/main/java/com/ken/config/WebAppDevConfig.java
+++ kir-web/src/main/java/com/ken/config/WebAppDevConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
