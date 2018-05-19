6d2b90f21569192a4495634cd5a86ee9ade8776f
kenn <bos@kenn.cn>
2018-05-19 12:11:59 +0800

    mondify activeMQ

---
+++ kir-queues/src/main/java/com/ken/QueueConsumer/Consumer.java
@@ -0,0 +1,14 @@
+package com.ken.QueueConsumer;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "sample.queue")
+    public void receiveQueue(String text) {
+        System.out.println("Consumer2="+text);
+    }
+
+}
